63b5df374a838acced1d3153fe795a45d899f9a0
Dhritabrata <dhritabratamitra@gmail.com>
2026-01-23 14:54:11 -0700

    Added new fields in the sign up form

--- backend/auth-service/internal/handlers/signup-handler.go
+++ backend/auth-service/internal/handlers/signup-handler.go
@@ -8,6 +8,8 @@ import (
 )
 
 type SignUpRequest struct {
+	FirstName       string `json:"firstName"`
+	LastName        string `json:"lastName"`
 	Username        string `json:"username"`
 	Email           string `json:"email"`
 	Password        string `json:"password"`
